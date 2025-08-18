01d6244a22c9df5cfb4e3354cfefcfc19d178edf
Kelvin Rotich <kelvinrotich416@gmail.com>
2025-08-18 19:11:44 +0300

    XML Config

--- src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/CodingNomad.java
@@ -11,16 +11,19 @@ public class CodingNomad {
     private final JDK jdk;
     private final IDE ide;
     private final Framework framework;
+    private final Monitor monitor;
 
     public String createAwesomeSoftware() {
         return MessageFormat.format(
                 "This coding nomad is creating awesome software using, "
-                        + "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5})",
+                        + "IDE:({0}:{1}), JDK: ({2}:{3}), Framework:({4}:{5}), Monitor: ({6}:{7})",
                 ide.getName(),
                 ide.getVersion(),
                 jdk.getName(),
                 jdk.getVersion(),
                 framework.getName(),
-                framework.getVersion());
+                framework.getVersion(),
+                monitor.getName(),
+                monitor.getSize());
     }
 }
---
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/Monitor.java
@@ -0,0 +1,13 @@
+package com.codingnomads.corespring.examples.application_context.xml;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class Monitor {
+    private String name;
+    private String size;
+}
